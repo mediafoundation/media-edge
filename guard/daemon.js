cc03da7dd2c1d0147bd4c3c6df6e381d8d929fd7
barboldev <barboldev@protonmail.com>
2023-04-08 17:19:19 -0300

    use new abis

--- guard/daemon.js
+++ guard/daemon.js
@@ -1,6 +1,6 @@
 const networks = require('./config/networks')
-const Resources = require('../evm-contract/build/contracts/Resources.json');
-const Marketplace = require('../evm-contract/build/contracts/Marketplace.json')
+const Resources = require('../media-evm-abis/Resources.json');
+const Marketplace = require('../media-evm-abis/Marketplace.json')
 const Web3 = require('web3');
 const models = require("./models");
 
