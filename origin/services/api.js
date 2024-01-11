dc78af6d25c47230fb7520e1137ea6282c733fa1
morgansson <17188509+morgansson@users.noreply.github.com>
2024-01-11 18:48:07 -0300

    Update api.js

--- origin/services/api.js
+++ origin/services/api.js
@@ -1,30 +1,141 @@
 const express = require('express')
 const { purgeRecord } = require('./varnish')
 const cors = require('cors')
-const app = express()
-app.use(cors())
-const port = 8080; // Change this to your desired port number
-//const networks = require("./../config/networks")
 const env = require("./../config/env")
 const {Signer} = require("media-sdk");
 const {DealsController} = require("../controllers/dealsController");
 const {PurgeLogsController} = require("../controllers/purgeLogsController");
 const {CaddyController} = require("../controllers/caddyController");
 const {generateUniqueDealId} = require("../utils/deals");
-const {generateNonce, SiweMessage} = require("siwe");
+const {generateNonce, SiweMessage, SiweErrorType} = require("siwe");
 const Session = require("express-session");
+
+/* const helmet = require('helmet'); */
+
+
+const app = express()
+
+/* app.use(
+  helmet({
+      contentSecurityPolicy: false,
+  }),
+); */
+
+app.use(cors({
+  origin: '*',
+  credentials: true,
+}))
+
+const port = 8080; // Change this to your desired port number
+//const networks = require("./../config/networks")
 
 const signer = new Signer()
 
 app.use(express.json())
 
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-store')
+  next()
+})
+
 app.use(Session({
-  name: 'siwe-quickstart',
-  secret: "siwe-quickstart-secret",
+  name: 'siwe-quickstarts',
+  secret: "siwe-quickstart-secrets",
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: false, sameSite: false }
+  cookie: { 
+    httpOnly: true,
+    secure: true
+ }
 }));
+
+app.get('/nonce', async (req, res) => {
+  req.session.nonce = generateNonce();
+  req.session.save(() => res.status(200).send(req.session.nonce).end());
+});
+
+app.post('/me', async (req, res) => {
+  const data = await checkSignature(req);
+
+  if (!data) {
+    console.log('Bad Signature')
+    res.status(401).json({ message: 'Bad Signature' });
+    return;
+  } else {
+    res.status(200)
+    .json({
+        address: req.body.message.address
+    })
+    .end();
+  }
+});
+
+async function checkSignature(req){
+  try {
+    const { signature, message } = req.body;
+    const siweMessage = new SiweMessage(message);
+    const data = await siweMessage.verify({ signature, nonce: req.session.nonce });
+    return data;
+  } catch (e) {
+    console.log(e)
+    return false;
+  }
+}
+
+app.post('/sign_in', async (req, res) => {
+  try {
+      const { signature } = req.body;
+      if (!req.body.message) {
+          res.status(422).json({ message: 'Expected signMessage object as body.' });
+          return;
+      }
+
+      const message = new SiweMessage(req.body.message);
+
+      const { data: fields} = await message.verify({ signature, nonce: req.session.nonce });
+
+      req.session.siwe = fields;
+      req.session.nonce = null;
+      req.session.cookie.expires = new Date(fields.expirationTime);
+      req.session.save(() =>
+          res
+              .status(200)
+              .json({
+                  address: req.session.siwe.address
+              })
+              .end(),
+      );
+  } catch (e) {
+      req.session.siwe = null;
+      req.session.nonce = null;
+      console.error(e);
+      switch (e) {
+          case SiweErrorType.EXPIRED_MESSAGE: {
+              req.session.save(() => res.status(440).json({ message: e.message }));
+              break;
+          }
+          case SiweErrorType.INVALID_SIGNATURE: {
+              req.session.save(() => res.status(422).json({ message: e.message }));
+              break;
+          }
+          default: {
+              req.session.save(() => res.status(500).json({ message: e.message }));
+              break;
+          }
+      }
+  }
+});
+
+app.post('/logout', async (req, res) => {
+  if (!req.session.siwe) {
+      res.status(401).json({ message: 'You have to first sign_in' });
+      return;
+  }
+
+  req.session.destroy(() => {
+      res.status(205).send();
+  });
+});
 
 // Define the endpoint for your remote function
 app.post('/', async (req, res) => {
@@ -108,55 +219,24 @@ app.get('/getAllDealsEndpoints', async (req, res) => {
   }
 })
 
-app.post('/getDealsEndpoints', async (req, res) => {
-  let payload = req.body
-  try{
-    const endpoints = {}
-    endpoints[payload.dealId] = await CaddyController.getHosts(payload.dealId)
-    res.send(endpoints)
-  } catch (e){
-    res.send(e)
+app.post('/getDealEndpoints', async (req, res) => {
+  const data = await checkSignature(req);
+
+  if (!data) {
+    console.log('Bad Signature')
+    res.status(401).json({ message: 'Bad Signature' });
+    return;
+  } else {
+    try{
+      const endpoints = {}
+      endpoints[req.body.dealId] = await CaddyController.getHosts(req.body.dealId)
+      res.send(endpoints)
+    } catch (e){
+      res.status(500).json({ message: 'Unknown error: '+ e });
+    }
   }
+
 })
-
-app.get('/nonce', function (_, res) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.send(generateNonce());
-});
-
-app.post('/verify', async function (req, res) {
-  try {
-    if (!req.body.message) {
-      res.status(422).json({ message: 'Expected prepareMessage object as body.' });
-      return;
-    }
-
-    let SIWEObject = new SiweMessage(req.body.message);
-    const { data: message } = await SIWEObject.verify({ signature: req.body.signature, nonce: req.session.nonce });
-
-    req.session.siwe = message;
-    req.session.cookie.expires = new Date(message.expirationTime);
-    req.session.save(() => res.status(200).send(true));
-  } catch (e) {
-    req.session.siwe = null;
-    req.session.nonce = null;
-    console.error(e);
-    switch (e) {
-      case ErrorTypes.EXPIRED_MESSAGE: {
-        req.session.save(() => res.status(440).json({ message: e.message }));
-        break;
-      }
-      case ErrorTypes.INVALID_SIGNATURE: {
-        req.session.save(() => res.status(422).json({ message: e.message }));
-        break;
-      }
-      default: {
-        req.session.save(() => res.status(500).json({ message: e.message }));
-        break;
-      }
-    }
-  }
-});
 
 
 // Start the server
