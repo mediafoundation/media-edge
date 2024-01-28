7e1a9e69bf8843488662665c5b735d1582175245
AgustinDiaz14 <67121345+AgustinDiaz14@users.noreply.github.com>
2024-01-28 19:02:47 -0300

    fix filterDomainsMatchingDeals

--- origin/utils/resources.js
+++ origin/utils/resources.js
@@ -12,7 +12,7 @@ const resourcesNotMatchingDeal = (resourcesIds, dealsIds) => {
 }
 
 function filterDomainsMatchingDeals(domains, dealIds) {
-    return domains.filter(domain => dealIds.includes(domain.dealId));
+    return domains.filter(domain => dealIds.includes(domain.dealId.toNumber()));
 }
 
 async function filterResourceDomains(resourceId, resourceDomains) {
